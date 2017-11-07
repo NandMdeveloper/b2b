a2c5e666216aab8ff086f5881379800251deeac7
develweb01 <javier.rodriguez@grupopro.com.ve>
2017-11-07 09:14:12 -0400

    Total dinamico y formato de monto y fechas

--- admin/aud/adminT.php
+++ admin/aud/adminT.php
@@ -8,7 +8,7 @@ $obj_pedidos= new class_pedidos;//LLAMADO A LA CLASE DE PEDIDOS
 
 $status=strip_tags($_GET['status']);
 $nombre=$_SESSION["nombre"];
-$team=$_SESSION["team"];
+//$team=$_SESSION["team"];
 $user=$_SESSION["user"];
 ?>
 <!DOCTYPE html>
@@ -94,17 +94,14 @@ $user=$_SESSION["user"];
                                         $arr_pedidos=$obj_pedidos->get_pedidos(2); ?>
                                         <?php for($i=0;$i<sizeof($arr_pedidos);$i++){
                                             $total+= $arr_pedidos[$i]['total_neto'];
+                                             $fecha = date_format(date_create($arr_pedidos[$i]['fec_emis']),'d/m/Y');
                                          ?>
                                             <tr class="odd gradeX">
                                                 <td><?php echo $arr_pedidos[$i]['doc_num']; ?></td>
                                                 <td><?php echo $arr_pedidos[$i]['cli_des']; ?></td>
                                                 <td><?php echo $arr_pedidos[$i]['nombre']; ?></td>
-                                                <td class="center">
-
-                                                    <b><span class="pull-right"><?php echo number_format($arr_pedidos[$i]['total_neto'], 2, ",", "."); ?></span></b>
-
-                                                    </td>
-                                                <td class="center"><?php echo $arr_pedidos[$i]['fec_emis']; ?></td>
+                                                <td class="text-right"><?php echo number_format($arr_pedidos[$i]['total_neto'], 2, ",", "."); ?></td>
+                                                <td class="center"><?php echo $fecha; ?></td>
                                                 <td class="center"><?php echo $arr_pedidos[$i]['comentario']; ?></td>
                                                 <td class="center">
                                                 <form action="detallePedido.php" method="POST">
@@ -191,17 +188,16 @@ $user=$_SESSION["user"];
                                         $arr_pedidos=$obj_pedidos->get_pedidos(); ?>
                                         <?php for($i=0;$i<sizeof($arr_pedidos);$i++){ 
                                                 $total+= $arr_pedidos[$i]['total_neto'];
+                                                $fecha = date_format(date_create($arr_pedidos[$i]['fec_emis']),'d/m/Y');
                                             ?>
                                             <tr class="odd gradeX">
                                                 <td><?php echo $arr_pedidos[$i]['doc_num']; ?></td>
                                                 <td><?php echo $arr_pedidos[$i]['cli_des']; ?></td>
                                                 <td><?php echo $arr_pedidos[$i]['nombre']; ?></td>
-                                                 <td class="center">
-                                                    
-                                                    <b><span class="pull-right"><?php echo number_format($arr_pedidos[$i]['total_neto'], 2, ",", "."); ?></span></b>
+                                                 <td class="text-right"><?php echo number_format($arr_pedidos[$i]['total_neto'], 2, ".", ","); ?></b>
 
                                                     </td>
-                                                <td class="center"><?php echo $arr_pedidos[$i]['fec_emis']; ?></td>
+                                                <td class="center"><?php echo  $fecha; ?></td>
                                                 <td class="center"><?php echo $arr_pedidos[$i]['comentario']; ?></td>
                                                 <td class="center">
                                                 <form action="detallePedido.php" method="POST">
@@ -213,6 +209,12 @@ $user=$_SESSION["user"];
                                     break;
                                 } ?>
                                     </tbody>
+                                        <tfoot>
+                                          <tr>
+                                            <th  colspan="3" style="text-align:right">Totales:</th>
+                                            <th colspan="4" ><span style="float:left;"id ='Base'>0</span></th>                                
+                                          </tr>
+                                        </tfoot>
                                 </table>
                             </div>
                             
@@ -300,11 +302,34 @@ $user=$_SESSION["user"];
 
     <!-- Page-Level Demo Scripts - Tables - Use for reference -->
     <script>
-    $(document).ready(function() {
-        $('#dataTables-example').DataTable({
-                responsive: true
+      $(document).ready(function() {
+        
+        $("#dataTables-example").DataTable( {
+          responsive: true,
+          "footerCallback": function ( row, data, start, end, display ) {
+                var api = this.api(), data;  
+                           var intVal = function ( i ) {
+                    return typeof i === 'string' ? i.replace(/[\$,\$.]/g, '')*1 : typeof i === 'number' ?  i : 0;
+                };
+
+                 Base = api.column( 3, { page: 'current'} ).data().reduce( function (a, b) { return intVal(a) + intVal(b);}, 0 );
+                             
+                Number.prototype.formatMoney = function(c, d, t){
+                var n = this, 
+                    c = isNaN(c = Math.abs(c)) ? 2 : c, 
+                    d = d == undefined ? "." : d, 
+                    t = t == undefined ? "," : t, 
+                    s = n < 0 ? "-" : "", 
+                    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+                    j = (j = i.length) > 3 ? j % 3 : 0;
+                   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+                 };
+                  Base = parseFloat(Math.round(Base) / 100);
+                   $('#Base').html(Base.formatMoney(2,'.',','));
+            }
         });
-    });
+
+      });
     </script>
 </body>
 
